333815e46e0e410c6ddeb0d2094b33fdb7ebd936
Georgi Kuklev <georgi.kuklev@protonmail.com>
2026-06-20 03:03:32 +0300

    Add utility for variable declarations

---
+++ Tests/SwiftStructOfArraysTests/VariableDeclSyntax+FlatBindingsTests.swift
@@ -0,0 +1,58 @@
+import SwiftSyntax
+import SwiftSyntaxBuilder
+import XCTest
+@testable import SwiftStructOfArraysMacros
+
+final class VariableDeclSyntaxFlatBindingsTests: XCTestCase {
+    func testSingleVariable() throws {
+        let code = "var animation: Int"
+        let decl = try VariableDeclSyntax(SyntaxNodeString(stringLiteral: code))
+        let flatDecls = decl.toFlatBindings().map { "\($0)" }
+
+        XCTAssertEqual(flatDecls, [code])
+    }
+    func testMultipleVariablesSingleType() throws {
+        let code = "var animation, index: Int"
+        let decl = try VariableDeclSyntax(SyntaxNodeString(stringLiteral: code))
+        let flatDecls = decl.toFlatBindings().map { "\($0)" }
+
+        let expectedDecls = [
+            "var animation: Int",
+            "var index: Int",
+        ]
+        XCTAssertEqual(flatDecls, expectedDecls)
+    }
+
+    func testMultipleVariablesMixedTypes() throws {
+        let decl = try VariableDeclSyntax("var animation, index: Int, name: String")
+        let flatDecls = decl.toFlatBindings().map { "\($0)" }
+
+        let expectedDecls = [
+            "var animation: Int",
+            "var index: Int",
+            "var name: String",
+        ]
+        XCTAssertEqual(flatDecls, expectedDecls)
+    }
+
+    func testFlatBindingsDropsInitValue() throws {
+        let decl = try VariableDeclSyntax("var index: Int = 0")
+        let flatDecls = decl.toFlatBindings().map { "\($0)" }
+
+        let expectedDecls = [ "var index: Int" ]
+
+        XCTAssertEqual(flatDecls, expectedDecls)
+    }
+
+    func testFlatBindingPreservesModifiers() throws {
+        let decl = try VariableDeclSyntax("private var index, position: Int")
+        let flatDecls = decl.toFlatBindings().map { "\($0)" }
+
+        let expectedDecls = [
+            "private var index: Int",
+            "private var position: Int",
+        ]
+
+        XCTAssertEqual(flatDecls, expectedDecls)
+    }
+}
