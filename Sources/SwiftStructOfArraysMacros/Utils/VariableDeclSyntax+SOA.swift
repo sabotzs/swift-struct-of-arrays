fc33cadbb944ad3a4d93f9ffcc09ecc5a6d366fa
Georgi Kuklev <georgi.kuklev@protonmail.com>
2026-06-21 17:08:09 +0300

    Update style

--- Sources/SwiftStructOfArraysMacros/Utils/VariableDeclSyntax+SOA.swift
+++ Sources/SwiftStructOfArraysMacros/Utils/VariableDeclSyntax+SOA.swift
@@ -5,17 +5,21 @@ extension VariableDeclSyntax {
     func toFlatBindings() -> [VariableDeclSyntax] {
         let specifier: Keyword = self.bindingSpecifier.tokenKind == .keyword(.let) ? .let : .var
         var typelessIds: [IdentifierPatternSyntax] = []
+
         return self.bindings.flatMap { binding -> [VariableDeclSyntax] in
             guard let identifier = binding.pattern.as(IdentifierPatternSyntax.self) else {
                 return []
             }
+
             guard var typeAnnotation = binding.typeAnnotation else {
                 typelessIds.append(identifier)
                 return []
             }
+
             typeAnnotation.trailingTrivia = .backslashes(0)
             typelessIds.append(identifier)
             defer { typelessIds.removeAll() }
+
             return typelessIds.map {
                 var name = PatternSyntax($0)
                 name.leadingTrivia = .space
