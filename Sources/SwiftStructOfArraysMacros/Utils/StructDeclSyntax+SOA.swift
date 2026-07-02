bf0d9915b101aa3acfa9745ac87dd843f7d9cd54
Georgi Kuklev <georgi.kuklev@protonmail.com>
2026-07-02 17:14:29 +0300

    Remove member block extension. Move impl to struct decl extension.

--- Sources/SwiftStructOfArraysMacros/Utils/StructDeclSyntax+SOA.swift
+++ Sources/SwiftStructOfArraysMacros/Utils/StructDeclSyntax+SOA.swift
@@ -4,10 +4,26 @@ import SwiftDiagnostics
 
 extension StructDeclSyntax {
     func getVariableDecls() -> [VariableDeclSyntax] {
-        return self.memberBlock.getVariableDecls()
+        return self.memberBlock.members.compactMap {
+            $0.decl.as(VariableDeclSyntax.self)
+        }
     }
 
     func getNestedTypeDecls() -> [DeclSyntaxProtocol] {
-        return self.memberBlock.getNestedTypeDecls()
+        let types: [DeclSyntaxProtocol.Type] = [
+            ActorDeclSyntax.self,
+            ClassDeclSyntax.self,
+            EnumDeclSyntax.self,
+            ProtocolDeclSyntax.self,
+            StructDeclSyntax.self,
+            TypeAliasDeclSyntax.self,
+        ]
+        return self.memberBlock.members.lazy
+            .map { $0.decl }
+            .filter { decl in
+                types.contains { type in
+                    decl.is(type)
+                }
+            }
     }
 }
